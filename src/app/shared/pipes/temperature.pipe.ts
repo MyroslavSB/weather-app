cb3617c0c2cc01c753a9348904a005da2fa88c9d
MyroslavSB <Myroslavsobchyshyn@gmail.com>
2024-04-07 17:20:33 +0200

    finished single day part

---
+++ src/app/shared/pipes/temperature.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'temperature',
+  standalone: true
+})
+export class TemperaturePipe implements PipeTransform {
+
+  transform(value: string | number): string {
+    return value + '°C';
+  }
+
+}
